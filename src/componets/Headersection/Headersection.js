10a2ecd22b0ba69de1e244c3a1ee4707a5669992
Masoumeh Rezai <you@example.com>
2023-07-17 21:37:27 +0430

    add some image at header section

--- src/componets/Headersection/Headersection.js
+++ src/componets/Headersection/Headersection.js
@@ -1,5 +1,12 @@
 import image1 from '../image/images.jpg';
 import image2 from '../image/images2.jpg';
+import image3 from '../image/images3.jpg';
+import image4 from '../image/images4.jpg';
+import image5 from '../image/images5.jpg';
+
+
+
+
 import classes from './Headersection.module.css'
 const Headersection = (props) => {
     return (
@@ -14,7 +21,7 @@ const Headersection = (props) => {
                     <p className={classes.texttwo}>Hello Masoumeh who are you?</p>
                 </div>
 
-    
+
 
             </div>
             <div className={classes.content}>
@@ -25,10 +32,83 @@ const Headersection = (props) => {
                     <span>Sara Rezai</span>
                     <p className={classes.texttwo}>Hello Masoumeh who are you?</p>
                 </div>
-
-                
-
             </div>
+
+
+            <div className={classes.content}>
+                <div >
+                    <img src={image3} alt="imageone" className={classes.img} />
+                </div>
+                <div className={classes.text}>
+                    <span>Sara Rezai</span>
+                    <p className={classes.texttwo}>Hello Masoumeh who are you?</p>
+                </div>
+            </div>
+
+
+            <div className={classes.content}>
+                <div >
+                    <img src={image4} alt="imageone" className={classes.img} />
+                </div>
+                <div className={classes.text}>
+                    <span>Sara Rezai</span>
+                    <p className={classes.texttwo}>Hello Masoumeh who are you?</p>
+                </div>
+            </div>
+
+             <div className={classes.content}>
+                <div >
+                    <img src={image5} alt="imageone" className={classes.img} />
+                </div>
+                <div className={classes.text}>
+                    <span>Sara Rezai</span>
+                    <p className={classes.texttwo}>Hello Masoumeh who are you?</p>
+                </div>
+            </div>
+
+
+             <div className={classes.content}>
+                <div >
+                    <img src={image2} alt="imageone" className={classes.img} />
+                </div>
+                <div className={classes.text}>
+                    <span>Sara Rezai</span>
+                    <p className={classes.texttwo}>Hello Masoumeh who are you?</p>
+                </div>
+            </div>
+
+             <div className={classes.content}>
+                <div >
+                    <img src={image2} alt="imageone" className={classes.img} />
+                </div>
+                <div className={classes.text}>
+                    <span>Sara Rezai</span>
+                    <p className={classes.texttwo}>Hello Masoumeh who are you?</p>
+                </div>
+            </div>
+
+
+            <div className={classes.content}>
+                <div >
+                    <img src={image2} alt="imageone" className={classes.img} />
+                </div>
+                <div className={classes.text}>
+                    <span>Sara Rezai</span>
+                    <p className={classes.texttwo}>Hello Masoumeh who are you?</p>
+                </div>
+            </div>
+
+             <div className={classes.content}>
+                <div >
+                    <img src={image2} alt="imageone" className={classes.img} />
+                </div>
+                <div className={classes.text}>
+                    <span>Sara Rezai</span>
+                    <p className={classes.texttwo}>Hello Masoumeh who are you?</p>
+                </div>
+            </div>
+
+
 
 
 
