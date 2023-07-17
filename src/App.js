64e034a1da7caf03d3173af45461ed170500bda3
Masoumeh Rezai <you@example.com>
2023-07-17 05:49:55 +0430

    frtst commit for this project

---
+++ src/App.js
@@ -0,0 +1,6 @@
+const App = () => {
+  return (
+    <h1>hi</h1>
+  )
+}
+export default App
---
+++ src/componets/Header/Header.js
@@ -0,0 +1,10 @@
+import { Fragment } from "react"
+
+const Header = (props) => {
+    return (
+        <Fragment>
+
+        </Fragment>
+    )
+}
+export default Header;
