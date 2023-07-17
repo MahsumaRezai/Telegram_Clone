765daa82333c51a0f8dff2945158f0ff7548bdfc
Masoumeh Rezai <you@example.com>
2023-07-18 04:14:32 +0430

    add some image in footer section

--- src/componets/Headersection/Headersection.js
+++ src/componets/Headersection/Headersection.js
@@ -4,113 +4,109 @@ import image3 from '../image/images3.jpg';
 import image4 from '../image/images4.jpg';
 import image5 from '../image/images5.jpg';
 import image6 from '../image/im.jpg'
-
 import classes from './Headersection.module.css'
+import { Fragment } from 'react';
+import Input from '../Input/input'
 const Headersection = (props) => {
     return (
-        <div className={classes.containre}>
-            {props.children}
-            <div className={classes.content}>
-                <div >
-                    <img src={image1} alt="imageone" className={classes.img} />
-                </div>
-                <div className={classes.text}>
-                    <span>Sara Rezai</span>
-                    <p className={classes.texttwo}>Hello Masoumeh who are you?</p>
+        <Fragment>
+
+            <div className={classes.containre}>
+                {props.children}
+                <div className={classes.input}>
+                    <Input />
                 </div>
 
+                <div className={classes.content}>
+                    <div >
+                        <img src={image1} alt="imageone" className={classes.img} />
+                    </div>
+                    <div className={classes.text}>
+                        <span>Sara Rezai</span>
+                        <p className={classes.texttwo}>Hello Masoumeh who are you?</p>
+                    </div>
 
 
-            </div>
-            <div className={classes.content}>
-                <div >
-                    <img src={image2} alt="imageone" className={classes.img} />
+
                 </div>
-                <div className={classes.text}>
-                    <span>cs class</span>
-                    <p className={classes.texttwo}>Do you have React project and I want it</p>
+                <div className={classes.content}>
+                    <div >
+                        <img src={image2} alt="imageone" className={classes.img} />
+                    </div>
+                    <div className={classes.text}>
+                        <span>cs class</span>
+                        <p className={classes.texttwo}>Do you have React project and I want it</p>
+                    </div>
                 </div>
-            </div>
 
 
-            <div className={classes.content}>
-                <div >
-                    <img src={image3} alt="imageone" className={classes.img} />
+                <div className={classes.content}>
+                    <div >
+                        <img src={image3} alt="imageone" className={classes.img} />
+                    </div>
+                    <div className={classes.text}>
+                        <span> Borther</span>
+                        <p className={classes.texttwo}>Hey Masoumeh where is my phone?</p>
+                    </div>
                 </div>
-                <div className={classes.text}>
-                    <span> Borther</span>
-                    <p className={classes.texttwo}>Hey Masoumeh where is my phone?</p>
+
+
+                <div className={classes.content}>
+                    <div >
+                        <img src={image4} alt="imageone" className={classes.img} />
+                    </div>
+                    <div className={classes.text}>
+                        <span>Father</span>
+                        <p className={classes.texttwo}>what is food for dinner?</p>
+                    </div>
                 </div>
-            </div>
 
-
-            <div className={classes.content}>
-                <div >
-                    <img src={image4} alt="imageone" className={classes.img} />
+                <div className={classes.content}>
+                    <div >
+                        <img src={image5} alt="imageone" className={classes.img} />
+                    </div>
+                    <div className={classes.text}>
+                        <span>Monkia cs class</span>
+                        <p className={classes.texttwo}>I want css class do you have?</p>
+                    </div>
                 </div>
-                <div className={classes.text}>
-                    <span>Father</span>
-                    <p className={classes.texttwo}>what is food for dinner?</p>
+
+
+                <div className={classes.content}>
+                    <div >
+                        <img src={image1} alt="imageone" className={classes.img} />
+                    </div>
+                    <div className={classes.text}>
+                        <span>Teacher</span>
+                        <p className={classes.texttwo}>where is your plan?</p>
+                    </div>
                 </div>
-            </div>
 
-            <div className={classes.content}>
-                <div >
-                    <img src={image5} alt="imageone" className={classes.img} />
+                <div className={classes.content}>
+                    <div >
+                        <img src={image2} alt="imageone" className={classes.img} />
+                    </div>
+                    <div className={classes.text}>
+                        <span>Sara Rezai</span>
+                        <p className={classes.texttwo}>Hello Masoumeh who are you?</p>
+                    </div>
                 </div>
-                <div className={classes.text}>
-                    <span>Monkia cs class</span>
-                    <p className={classes.texttwo}>I want css class do you have?</p>
+
+
+                <div className={classes.content}>
+                    <div >
+                        <img src={image6} alt="imageone" className={classes.img} />
+                    </div>
+                    <div className={classes.text}>
+                        <span>Sara Rezai</span>
+                        <p className={classes.texttwo}>Hello Masoumeh who are you?</p>
+                    </div>
                 </div>
-            </div>
-
-
-            <div className={classes.content}>
-                <div >
-                    <img src={image1} alt="imageone" className={classes.img} />
-                </div>
-                <div className={classes.text}>
-                    <span>Teacher</span>
-                    <p className={classes.texttwo}>where is your plan?</p>
-                </div>
-            </div>
-
-            <div className={classes.content}>
-                <div >
-                    <img src={image2} alt="imageone" className={classes.img} />
-                </div>
-                <div className={classes.text}>
-                    <span>Sara Rezai</span>
-                    <p className={classes.texttwo}>Hello Masoumeh who are you?</p>
-                </div>
-            </div>
-
-
-            <div className={classes.content}>
-                <div >
-                    <img src={image6} alt="imageone" className={classes.img} />
-                </div>
-                <div className={classes.text}>
-                    <span>Sara Rezai</span>
-                    <p className={classes.texttwo}>Hello Masoumeh who are you?</p>
-                </div>
-            </div>
-
-            <div className={classes.content}>
-                <div >
-                    <img src={image2} alt="imageone" className={classes.img} />
-                </div>
-                <div className={classes.text}>
-                    <span>Asal</span>
-                    <p className={classes.texttwo}>Hello Masoumeh who are you?</p>
-                </div>
-            </div>
 
 
 
-
-
-        </div>
+            </div >
+        </Fragment>
 
     )
 }
---
+++ src/componets/Input/input.js
@@ -0,0 +1,8 @@
+const Input=(props)=>{
+    return(
+        <div>
+        <input placeholder="Search"></input>
+            </div>
+    )
+}
+export default Input ;
