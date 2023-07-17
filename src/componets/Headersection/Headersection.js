d6c9a751e36ce8dea1ef822314a26bbecc62378a
Masoumeh Rezai <you@example.com>
2023-07-17 11:03:38 +0430

    add section left

--- src/componets/Headersection/Headersection.js
+++ src/componets/Headersection/Headersection.js
@@ -1,13 +1,33 @@
-
+import image1 from '../image/images.jpg';
+import image2 from '../image/images2.jpg';
+import classes from './Headersection.module.css'
 const Headersection = (props) => {
     return (
-        <div>
+        <div className={classes.containre}>
             {props.children}
-            <div>
-                <div>
-                    
+            <div className={classes.content}>
+                <div >
+                    <img src={image1} alt="imageone" className={classes.img} />
                 </div>
-                <div></div>
+                <div className={classes.text}>
+                    <span>Sara Rezai</span>
+                    <p className={classes.texttwo}>Hello Masoumeh who are you?</p>
+                </div>
+
+    
+
+            </div>
+            <div className={classes.content}>
+                <div >
+                    <img src={image2} alt="imageone" className={classes.img} />
+                </div>
+                <div className={classes.text}>
+                    <span>Sara Rezai</span>
+                    <p className={classes.texttwo}>Hello Masoumeh who are you?</p>
+                </div>
+
+                
+
             </div>
 
 
